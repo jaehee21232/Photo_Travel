52ab96243add9a94a2d942d7103bf1a8aaa463a5
jaehee21232 <a62533196@gmail.com>
2023-11-05 23:57:30 +0900

    기상청 웹 크롤링

---
+++ lib/common/weather_crawling.dart
@@ -0,0 +1,29 @@
+import 'package:http/http.dart' as http;
+import 'package:html/dom.dart';
+import 'package:html/parser.dart' as parser;
+import 'package:photo_travel/models/weather_model.dart';
+
+Future<WeatherData> getWeatherData(String name) async {
+  var uri =
+      Uri.parse('https://www.weather.go.kr/w/obs-climate/land/city-obs.do');
+  final response = await http.get(uri);
+  Document document = parser.parse(response.body);
+  List<Element> keywordElements =
+      document.querySelectorAll('#weather_table')[0].getElementsByTagName('tr');
+  keywordElements.removeRange(0, 2);
+  //print(keywordElements[0].getElementsByTagName('td')[0].text);
+  final data = keywordElements
+      .where(
+        (Element element) {
+          return element.getElementsByTagName('td')[0].text == name;
+        },
+      )
+      .toList()[0]
+      .getElementsByTagName('td');
+  return WeatherData(
+      name: data[0].text,
+      vs: double.parse(data[2].text),
+      ta: double.parse(data[5].text),
+      pta: double.parse(data[7].text),
+      rn: double.parse(data[8].text));
+}
