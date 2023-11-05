e44c1290b708c5724ebc4fda6333c4a86ffd3282
jaehee21232 <a62533196@gmail.com>
2023-11-05 15:13:26 +0900

    더미 배너 데이터

---
+++ lib/common/data.dart
@@ -0,0 +1,11 @@
+import 'package:banner_carousel/banner_carousel.dart';
+
+final test =
+    'https://www.adobe.com/content/dam/cc/us/en/creative-cloud/photography/discover/landscape-photography/CODERED_B1_landscape_P2d_714x348.jpg.img.jpg';
+
+List<BannerModel> listBanners = [
+  BannerModel(imagePath: test, id: "1"),
+  BannerModel(imagePath: test, id: "2"),
+  BannerModel(imagePath: test, id: "3"),
+  BannerModel(imagePath: test, id: "4"),
+];
