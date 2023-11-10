0956861ef0ee8c5fe2a0d10ea3352cb9901b1459
jaehee21232 <a62533196@gmail.com>
2023-11-11 03:38:04 +0900

    회원가입 라우터추가

--- lib/common/router.dart
+++ lib/common/router.dart
@@ -2,6 +2,7 @@ import 'package:go_router/go_router.dart';
 import 'package:photo_travel/main.dart';
 import 'package:photo_travel/screen/home_screen.dart';
 import 'package:photo_travel/screen/logo_screen.dart';
+import 'package:photo_travel/screen/register_screen.dart';
 import 'package:photo_travel/screen/tab_screen.dart';
 import 'package:photo_travel/widget/weather.dart';
 
@@ -12,18 +13,23 @@ final GoRouter router = GoRouter(
       builder: (context, state) => const LogoScreen(),
     ),
     GoRoute(
-        path: '/tab-main',
-        builder: (context, state) => const TabMainScreen(),
-        routes: [
-          GoRoute(
-              path: 'home',
-              builder: (context, state) => const HomeScreen(),
-              routes: [
-                GoRoute(
-                  path: 'weather',
-                  builder: (context, state) => const WeatherWidget(),
-                )
-              ]),
-        ]),
+      path: '/tab-main',
+      builder: (context, state) => const TabMainScreen(),
+      routes: [
+        GoRoute(
+            path: 'home',
+            builder: (context, state) => const HomeScreen(),
+            routes: [
+              GoRoute(
+                path: 'weather',
+                builder: (context, state) => const WeatherWidget(),
+              )
+            ]),
+      ],
+    ),
+    GoRoute(
+      path: '/register',
+      builder: (context, state) => const RegisterScreen(),
+    ),
   ],
 );
