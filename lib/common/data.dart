9f4a03a03edb7d1a851039cbf2a0a0c1d0282b26
jaehee21232 <a62533196@gmail.com>
2023-11-11 03:38:15 +0900

    스토리지 추가

--- lib/common/data.dart
+++ lib/common/data.dart
@@ -1,6 +1,9 @@
 import 'package:banner_carousel/banner_carousel.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-final test =
+const storage = FlutterSecureStorage();
+const test =
     'https://www.adobe.com/content/dam/cc/us/en/creative-cloud/photography/discover/landscape-photography/CODERED_B1_landscape_P2d_714x348.jpg.img.jpg';
 
 List<BannerModel> listBanners = [
